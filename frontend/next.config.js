e8b6fc18f9a7dedc7f17ea987e49e4ec2bb72d0a
Ani_03 <140332086+Anidipta@users.noreply.github.com>
2025-03-23 19:06:10 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,37 +1,36 @@
-module.exports = {
-  // ...existing code...
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains:
-     [
-        'glad.geog.umd.edu',
-        'raw.githubusercontent.com',
-        '/python-pillow.github.io',
-        'streamlit.io',
-        'www.svgrepo.com',
-        'velog.velcdn.com',
-        'upload.wikimedia.org',
-        'logowik.com',
-        'github.com',
-        's3.amazonaws.com',
-        'icon.icepanel.io',
-        'python-pillow.github.io',
-        'www.gstatic.com',
-        'geog-312.gishub.org',
-        'image.pngaaa.com',
-        'www.pngitem.com',
-        'www.pngkey.com',
-        'www.svgrepo.com',
-        'img.etimg.com',
-        'i.pinimg.com',
-        'logohistory.net',
-        'raw.githubusercontent.com',
-        'www.google.com',
-        'dash.plotly.com',
-        'e7.pngegg.com',
-        'powerbi.microsoft.com',
-        'encrypted-tbn0.gstatic.com',
-        'rasterio.readthedocs.io'
-      ]
-  },
-  // ...existing code...
+    domains: [
+      'glad.geog.umd.edu',
+      'raw.githubusercontent.com',
+      'python-pillow.github.io',
+      'streamlit.io',
+      'www.svgrepo.com',
+      'velog.velcdn.com',
+      'upload.wikimedia.org',
+      'logowik.com',
+      'github.com',
+      's3.amazonaws.com',
+      'icon.icepanel.io',
+      'www.gstatic.com',
+      'geog-312.gishub.org',
+      'image.pngaaa.com',
+      'www.pngitem.com',
+      'www.pngkey.com',
+      'img.etimg.com',
+      'i.pinimg.com',
+      'logohistory.net',
+      'www.google.com',
+      'dash.plotly.com',
+      'e7.pngegg.com',
+      'powerbi.microsoft.com',
+      'encrypted-tbn0.gstatic.com',
+      'rasterio.readthedocs.io'
+    ]
+  }
 };
+
+module.exports = nextConfig;
